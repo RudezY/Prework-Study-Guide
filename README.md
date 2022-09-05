8834ecf461cf9d02e082a9caaad3af271abcdc41
RudezY <rudyyamashiro@yahoo.com>
2022-09-05 16:27:21 -0700

    Added info to the readme markdown

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Prework-Study-Guide
-A study guide for course pre-work
+# Prework Study Guide Webpag
+
+## Description
+
+This Prework Study Guide was created for bootcamp students who are completing the Prework. It was created to help learn the basics to coding using HTML, CSS, Git, and Javascript. Building this project helped get a basic understanding of what will be learned in this bootcamp and basic idea of troubleshooting.
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this prework study guide, you can review the notes made in each section. If you want to randomly study a topic open Chrome Devtools and inspect the console portion to receive a randoom topic.
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-<<<<<<< HEAD
-var randomTopic = topics[Math.floor(Math.random() * topics.length)];
-
 function listTopics() {
   for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
@@ -31,29 +28,4 @@ listTopics();
 
 console.log('Which topic should we study first?');
 
-=======
-function listTopics() {
- for (var x = 0; x < topics.length; x++) {
-   console.log(topics[x]);
- }
-}
-
-function selectTopic() {
- if (randomTopic === 'HTML') {
-   console.log("Let's study HTML!");
- } else if (randomTopic === 'CSS') {
-   console.log("Let's study CSS!");
- } else if (randomTopic === 'Git') {
-   console.log("Let's study Git!");
- } else if (randomTopic === 'JavaScript') {
-   console.log("Let's study JavaScript!");
- } else {
-   console.log('Please try again!');
- }
-}
-
-console.log('Here are the topics we learned through Prework:');
-listTopics();
-console.log('Which topic should we study first?');
->>>>>>> b2611e2534ef817ec48408d88c98347c46f801ee
 selectTopic();
